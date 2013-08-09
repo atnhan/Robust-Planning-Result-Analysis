c83f3a3f48a2a098c1254e2fc89e9ee48d4a896c
Tuan Nguyen <atnhan@gmail.com>
2013-08-08 22:54:18 -0700

    m

---
+++ README.md
@@ -0,0 +1,1 @@
+1) Go to pre-process, run process-default-raw-data. This will read results output by DeFault, write them into csv and move them to csv/DeFault
--- r-scripts/robustness_comparison.R
+++ r-scripts/robustness_comparison.R
@@ -1,4 +1,6 @@
 require("data.table")
+require("ggplot2")
+require("reshape")
 
 approaches <- c("pure_ff", "default")
 domains <- c("satellite", "freecell")
@@ -38,21 +40,101 @@ get_problems <- function() {
   return (result)
 }
 
-freecell_lower_pure_ff <- data.table(read.csv("../csv/PISA/pure_ff/freecell@lower@pure_ff.csv", comment.char='#'))
-freecell_default <- data.table(read.csv("../csv/DeFault//freecell@default.csv", comment.char='#'))
-
-# Maximum robust plans for each instances
-robust_plans_freecell_lower_pure_ff <- freecell_lower_pure_ff[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
-robust_plans_freecell_default <- freecell_default[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
-
-# Maximum robust plans returned by two approaches
-robust_plans_freecell_lower_pure_ff_AND_default <- merge(robust_plans_freecell_lower_pure_ff, robust_plans_freecell_default, 
-                                                         by=c("domain","problem"))
-
-robustness_by_lower_pure_ff <- robust_plans_freecell_lower_pure_ff_AND_default[robust_plans_freecell_lower_pure_ff_AND_default$max_robustness.x]
-
-plans_comparison <- function() {
+# Input:
+# + domain_list: list of domain names. Ex: domain_list <- c("zenotravel", "depots", "rover")
+# + file_for_first_approach: csv files produced by the first approach for each domain above.
+# Example: file_for_first_approach <- c("../csv/PISA/inc_robust/zenotravel@lower@inc_robust.csv",
+#                                       "../csv/PISA/inc_robust/depots@lower@inc_robust.csv",
+#                                       "../csv/PISA/inc_robust/rover@lower@inc_robust.csv")
+# + file_for_second_approach: csv files produced by the second approach for each domain above.
+# Example: file_for_second_approach <- c("../csv/PISA/pure_ff/zenotravel@lower@pure_ff.csv",
+#                                        "../csv/PISA/pure_ff/depots@lower@pure_ff.csv",
+#                                        "../csv/PISA/pure_ff/rover@lower@pure_ff.csv")
+# Note: the two file list must have the same length
+# + ouput_file: csv output file
+robust_plans_comparison <- function(domain_list,
+                                    file_for_first_approach,
+                                    file_for_second_approach,
+                                    output_file) {
+  N <- length(domain_list)
   
+  summary_table <- data.table(domain=rep("", N), better=rep(0, N),
+                              equal=rep(0, N), worse=rep(0, N))
+  row_count <- 0
+  
+  for (i in 1:length(file_for_first_approach)) {
+    first_table <- data.table(read.csv(file_for_first_approach[i], comment.char='#'))
+    second_table <- data.table(read.csv(file_for_second_approach[i], comment.char='#'))
+    
+    robust_plans_first_table <- first_table[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
+    robust_plans_second_table <- second_table[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
+    
+    d <- merge(robust_plans_first_table, robust_plans_second_table, all.x = TRUE, by=c("domain","problem"))
+    d[is.na(d)] <- 0
+    better_count = nrow(d[d$max_robustness.x > d$max_robustness.y])
+    #print(better_count)
+    
+    d <- merge(robust_plans_first_table, robust_plans_second_table, by=c("domain","problem"))
+    equal_count = nrow(d[d$max_robustness.x == d$max_robustness.y])
+    #print(equal_count)
+    
+    d <- merge(robust_plans_first_table, robust_plans_second_table, all.y = TRUE, by=c("domain","problem"))
+    d[is.na(d)] <- 0
+    worse_count = nrow(d[d$max_robustness.x < d$max_robustness.y])
+    #print(worse_count)
+    
+    row_count <- row_count + 1
+    summary_table[row_count,] <- list(domain_list[i],better_count, equal_count, worse_count)
+  }
+  
+  write.csv(summary_table, output_file)
 }
+
+domain_list <- c("zenotravel", "depots", "rover", "driverlog", "freecell", "satellite")
+
+file_for_lower_inc_robust <- c("../csv/PISA/inc_robust/zenotravel@lower@inc_robust.csv",
+                               "../csv/PISA/inc_robust/depots@lower@inc_robust.csv",
+                               "../csv/PISA/inc_robust/rover@lower@inc_robust.csv",
+                               "../csv/PISA/inc_robust/driverlog@lower@inc_robust.csv",
+                               "../csv/PISA/inc_robust/freecell@lower@inc_robust.csv",
+                               "../csv/PISA/inc_robust/satellite@lower@inc_robust.csv")
+
+file_for_lower_pure_ff <- c("../csv/PISA/pure_ff/zenotravel@lower@pure_ff.csv",
+                            "../csv/PISA/pure_ff/depots@lower@pure_ff.csv",
+                            "../csv/PISA/pure_ff/rover@lower@pure_ff.csv",
+                            "../csv/PISA/pure_ff/driverlog@lower@pure_ff.csv",
+                            "../csv/PISA/pure_ff/freecell@lower@pure_ff.csv",
+                            "../csv/PISA/pure_ff/satellite@lower@pure_ff.csv")
+
+file_for_upper_inc_robust <- c("../csv/PISA/inc_robust/zenotravel@upper@inc_robust.csv",
+                               "../csv/PISA/inc_robust/depots@upper@inc_robust.csv",
+                               "../csv/PISA/inc_robust/rover@upper@inc_robust.csv",
+                               "../csv/PISA/inc_robust/driverlog@upper@inc_robust.csv",
+                               "../csv/PISA/inc_robust/freecell@upper@inc_robust.csv",
+                               "../csv/PISA/inc_robust/satellite@upper@inc_robust.csv")
+
+file_for_upper_pure_ff <- c("../csv/PISA/pure_ff/zenotravel@upper@pure_ff.csv",
+                            "../csv/PISA/pure_ff/depots@upper@pure_ff.csv",
+                            "../csv/PISA/pure_ff/rover@upper@pure_ff.csv",
+                            "../csv/PISA/pure_ff/driverlog@upper@pure_ff.csv",
+                            "../csv/PISA/pure_ff/freecell@upper@pure_ff.csv",
+                            "../csv/PISA/pure_ff/satellite@upper@pure_ff.csv")
+
+file_for_default <- c("../csv/DeFault/zenotravel@default.csv",
+                      "../csv/DeFault/depots@default.csv",
+                      "../csv/DeFault/rover@default.csv",
+                      "../csv/DeFault/driverlog@default.csv",
+                      "../csv/DeFault/freecell@default.csv",
+                      "../csv/DeFault/satellite@default.csv")
+
+robust_plans_comparison(domain_list[1:4], file_for_lower_inc_robust[1:4], file_for_lower_pure_ff[1:4], "lower_inc_robust_VS_pure_ff.csv")
+
+robust_plans_comparison(domain_list[1:4], file_for_upper_inc_robust[1:4], file_for_upper_pure_ff[1:4], "upper_inc_robust_VS_pure_ff.csv")
+
+#robust_plans_comparison(domain_list[5], file_for_default[5], file_for_upper_pure_ff[5], "default_vs_upper_pure_ff.csv")
+
+
+
+
 
 
