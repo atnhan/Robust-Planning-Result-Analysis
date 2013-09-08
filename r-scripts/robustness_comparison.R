e5f38cc62fefcb2629917da6024c882b97152e7b
Tuan Nguyen <atnhan@gmail.com>
2013-09-07 17:39:56 -0700

    m

--- r-scripts/robustness_comparison.R
+++ r-scripts/robustness_comparison.R
@@ -76,7 +76,7 @@ get_problems <- function() {
 #                                        "../csv/PISA/pure_ff/rover@lower@pure_ff.csv")
 # Note: the two file list must have the same length
 # + ouput_file: csv output file
-robust_plans_comparison <- function(domain_list,
+num_max_robustness_instances_comparison <- function(domain_list,
                                     file_for_first_approach,
                                     file_for_second_approach,
                                     output_file) {
@@ -93,30 +93,30 @@ robust_plans_comparison <- function(domain_list,
     robust_plans_first_table <- first_table[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
     robust_plans_second_table <- second_table[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
     
-    d <- merge(robust_plans_first_table, robust_plans_second_table, all.x = TRUE, by=c("domain","problem"))
-    d[is.na(d)] <- 0
-    better_count = nrow(d[d$max_robustness.x > d$max_robustness.y])
-    #print(better_count)
-    
-    d <- merge(robust_plans_first_table, robust_plans_second_table, by=c("domain","problem"))
-    equal_count = nrow(d[d$max_robustness.x == d$max_robustness.y])
-    #print(equal_count)
-    
-    d <- merge(robust_plans_first_table, robust_plans_second_table, all.y = TRUE, by=c("domain","problem"))
-    d[is.na(d)] <- 0
-    worse_count = nrow(d[d$max_robustness.x < d$max_robustness.y])
-    #print(worse_count)
-    
-#     # COUNTING BASED ONLY ON INSTANCES SOLVED BY BOTH THE TWO APPROACHES. USE WHEN WE DONT HAVE ALL INSTANCES.
-#     d <- merge(robust_plans_first_table, robust_plans_second_table, by=c("domain","problem"))
+#     d <- merge(robust_plans_first_table, robust_plans_second_table, all.x = TRUE, by=c("domain","problem"))
+#     d[is.na(d)] <- 0
 #     better_count = nrow(d[d$max_robustness.x > d$max_robustness.y])
 #     #print(better_count)
-# 
+#     
+#     d <- merge(robust_plans_first_table, robust_plans_second_table, by=c("domain","problem"))
 #     equal_count = nrow(d[d$max_robustness.x == d$max_robustness.y])
 #     #print(equal_count)
 #     
+#     d <- merge(robust_plans_first_table, robust_plans_second_table, all.y = TRUE, by=c("domain","problem"))
+#     d[is.na(d)] <- 0
 #     worse_count = nrow(d[d$max_robustness.x < d$max_robustness.y])
 #     #print(worse_count)
+    
+    # COUNTING BASED ONLY ON INSTANCES SOLVED BY BOTH THE TWO APPROACHES. USE WHEN WE DONT HAVE ALL INSTANCES.
+    d <- merge(robust_plans_first_table, robust_plans_second_table, by=c("domain","problem"))
+    better_count = nrow(d[d$max_robustness.x > d$max_robustness.y])
+    #print(better_count)
+
+    equal_count = nrow(d[d$max_robustness.x == d$max_robustness.y])
+    #print(equal_count)
+    
+    worse_count = nrow(d[d$max_robustness.x < d$max_robustness.y])
+    #print(worse_count)
     
     
     
@@ -127,46 +127,279 @@ robust_plans_comparison <- function(domain_list,
   write.csv(summary_table, output_file)
 }
 
-max_robustness_comparison <- function(domain_list,
-                                    file_for_first_approach,
-                                    file_for_second_approach,
-                                    output_file) {
+# # Compare the maximal robustness returned by two approaches
+# max_robustness_comparison <- function(domain_list,
+#                                     file_for_first_approach,
+#                                     file_for_second_approach) {
+#   # Number of domains
+#   N <- length(domain_list)
+#   
+#   for (i in 1:N) {
+#     # Read result files produced by each approach for this domain
+#     first_table <- data.table(read.csv(file_for_first_approach[i], comment.char='#'))
+#     second_table <- data.table(read.csv(file_for_second_approach[i], comment.char='#'))
+#     
+#     # Extract for each instance (incomplete domain + problem) the maximal robustness
+#     max_robustness_first_table <- first_table[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
+#     max_robustness_second_table <- second_table[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
+#     
+#     # Merge the two tables, only consider instances both approach produce plans
+#     d <- merge(max_robustness_first_table, max_robustness_second_table, by=c("domain","problem"))
+#     
+#     
+#     output <- paste(domain_list[i],"_MAX_ROBUST.csv",sep="")
+#     write.csv(d, output)
+#   }
+# }
+
+
+# Compare the number of plans returned by two approaches
+num_plans_comparison <- function(domain_list,
+                                      file_for_first_approach,
+                                      file_for_second_approach, output_file) {
+  # Number of domains
   N <- length(domain_list)
   
   summary_table <- data.table(domain=rep("", N), better=rep(0, N),
                               equal=rep(0, N), worse=rep(0, N))
   row_count <- 0
   
-  for (i in 1:length(file_for_first_approach)) {
+  for (i in 1:N) {
+    # Read result files produced by each approach for this domain
     first_table <- data.table(read.csv(file_for_first_approach[i], comment.char='#'))
     second_table <- data.table(read.csv(file_for_second_approach[i], comment.char='#'))
     
-    max_robustness_first_table <- first_table[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
-    max_robustness_second_table <- second_table[, list(max_robustness = max(plan_robustness)), by=list(domain, problem)]
+    # Extract for each instance (incomplete domain + problem) the maximal robustness
+    num_plans_first_table <- first_table[, list(num_plans = max(plan_id)), by=list(domain, problem)]
+    num_plans_second_table <- second_table[, list(num_plans = max(plan_id)), by=list(domain, problem)]
     
-        # COUNTING BASED ONLY ON INSTANCES SOLVED BY BOTH THE TWO APPROACHES. USE WHEN WE DONT HAVE ALL INSTANCES.
-        d <- merge(robust_plans_first_table, robust_plans_second_table, by=c("domain","problem"))
-        better_count = nrow(d[d$max_robustness.x > d$max_robustness.y])
-        #print(better_count)
+    # Merge the two tables, only consider instances for which both approaches produce plans
+    d <- merge(num_plans_first_table, num_plans_second_table, all.x = TRUE, by=c("domain","problem"))
+    d[is.na(d)] <- 0
+    better_count = nrow(d[d$num_plans.x > d$num_plans.y])    
+    d <- merge(num_plans_first_table, num_plans_second_table, by=c("domain","problem"))
+    equal_count = nrow(d[d$num_plans.x == d$num_plans.y])
+    d <- merge(num_plans_first_table, num_plans_second_table, all.y = TRUE, by=c("domain","problem"))
+    d[is.na(d)] <- 0
+    worse_count = nrow(d[d$num_plans.x < d$num_plans.y])
     
-        equal_count = nrow(d[d$max_robustness.x == d$max_robustness.y])
-        #print(equal_count)
-        
-        worse_count = nrow(d[d$max_robustness.x < d$max_robustness.y])
-        #print(worse_count)
     
-    
+#     d <- merge(num_plans_first_table, num_plans_second_table, by=c("domain","problem"))
+#     better_count = nrow(d[d$num_plans.x > d$num_plans.y])    
+#     equal_count = nrow(d[d$num_plans.x == d$num_plans.y])
+#     worse_count = nrow(d[d$num_plans.x < d$num_plans.y])
     
     row_count <- row_count + 1
     summary_table[row_count,] <- list(domain_list[i],better_count, equal_count, worse_count)
   }
   
   write.csv(summary_table, output_file)
+  
+}
+
+mean_plan_length_comparison <- function(domain_list,
+                                 file_for_first_approach,
+                                 file_for_second_approach, output_file) {
+  # Number of domains
+  N <- length(domain_list)
+  
+  summary_table <- data.table(domain=rep("", N), shorter=rep(0, N),
+                              equal=rep(0, N), longer=rep(0, N))
+  row_count <- 0
+  
+  for (i in 1:N) {
+    # Read result files produced by each approach for this domain
+    first_table <- data.table(read.csv(file_for_first_approach[i], comment.char='#'))
+    second_table <- data.table(read.csv(file_for_second_approach[i], comment.char='#'))
+    
+    # Extract for each instance (incomplete domain + problem) the maximal robustness
+    mean_plan_length_first_table <- first_table[, list(mean_plan_length = mean(plan_length)), by=list(domain, problem)]
+    mean_plan_length_second_table <- second_table[, list(mean_plan_length = mean(plan_length)), by=list(domain, problem)]
+    
+    # Merge the two tables, only consider instances for which both approaches produce plans
+    d <- merge(mean_plan_length_first_table, mean_plan_length_second_table, by=c("domain","problem"))
+    shorter_count = nrow(d[d$mean_plan_length.x < d$mean_plan_length.y])    
+    equal_count = nrow(d[d$mean_plan_length.x == d$mean_plan_length.y])
+    longer_count = nrow(d[d$mean_plan_length.x > d$mean_plan_length.y])
+    
+    row_count <- row_count + 1
+    summary_table[row_count,] <- list(domain_list[i],shorter_count, equal_count, longer_count)
+    
+    #out <- paste("mean_plan_length@", domain_list[i],sep="")
+    #write.csv(d, out)
+  }
+  
+  write.csv(summary_table, output_file)
+  
 }
 
 
-#robust_plans_comparison(domain_list[1:4], file_for_lower_annotations_free_ff[1:4], file_for_lower_inc_robust[1:4], "../summary_tables/lower_annotations_free_ff_VS_inc_robust.csv")
+length_of_equal_robustness_comparison <- function(domain_list,
+                                        file_for_first_approach,
+                                        file_for_second_approach, output_file) {
+  # Number of domains
+  N <- length(domain_list)
+  
+  summary_table <- data.table(domain=rep("", N), shorter=rep(0, N),
+                              equal=rep(0, N), longer=rep(0, N))
+  row_count <- 0
+  
+  for (i in 1:N) {
+    # Read result files produced by each approach for this domain
+    first_table <- data.table(read.csv(file_for_first_approach[i], comment.char='#'))
+    second_table <- data.table(read.csv(file_for_second_approach[i], comment.char='#'))
+    
+    # Extract for each instance (incomplete domain + problem) the maximal robustness
+    mean_plan_length_first_table <- first_table[, list(mean_plan_length = mean(plan_length)), by=list(domain, problem)]
+    mean_plan_length_second_table <- second_table[, list(mean_plan_length = mean(plan_length)), by=list(domain, problem)]
+    
+    # Merge the two tables, only consider instances for which both approaches produce plans
+    d <- merge(mean_plan_length_first_table, mean_plan_length_second_table, by=c("domain","problem"))
+    shorter_count = nrow(d[d$mean_plan_length.x < d$mean_plan_length.y])    
+    equal_count = nrow(d[d$mean_plan_length.x == d$mean_plan_length.y])
+    longer_count = nrow(d[d$mean_plan_length.x > d$mean_plan_length.y])
+    
+    row_count <- row_count + 1
+    summary_table[row_count,] <- list(domain_list[i],shorter_count, equal_count, longer_count)
+    
+    #out <- paste("mean_plan_length@", domain_list[i],sep="")
+    #write.csv(d, out)
+  }
+  
+  write.csv(summary_table, output_file)
+  
+}
 
-#robust_plans_comparison(domain_list[5:6], file_for_lower_inc_robust[5:6], file_for_default[5:6], "../summary_tables/lower_inc_robust_VS_default.csv")
+time_for_first_plan_comparison <- function(domain_list,
+                                           file_for_first_approach,
+                                           file_for_second_approach, output_file) {
+  # Number of domains
+  N <- length(domain_list)
+  
+  summary_table <- data.table(domain=rep("", N), faster=rep(0, N),
+                              equal=rep(0, N), slower=rep(0, N))
+  row_count <- 0
+  
+  for (i in 1:N) {
+    # Read result files produced by each approach for this domain
+    first_table <- data.table(read.csv(file_for_first_approach[i], comment.char='#'))
+    second_table <- data.table(read.csv(file_for_second_approach[i], comment.char='#'))
+    
+    # Extract for each instance (incomplete domain + problem) the maximal robustness
+    time_first_plan_first_table <- first_table[, list(time_first_plan = min(total_time)), by=list(domain, problem)]
+    time_first_plan_second_table <- second_table[, list(time_first_plan = min(total_time)), by=list(domain, problem)]
+    
+    # Merge the two tables, only consider instances for which both approaches produce plans
+    d <- merge(time_first_plan_first_table, time_first_plan_second_table, by=c("domain","problem"))
+    faster_count = nrow(d[d$time_first_plan.x < d$time_first_plan.y])    
+    equal_count = nrow(d[d$time_first_plan.x == d$time_first_plan.y])
+    slower_count = nrow(d[d$time_first_plan.x > d$time_first_plan.y])
+    
+    row_count <- row_count + 1
+    summary_table[row_count,] <- list(domain_list[i],faster_count, equal_count, slower_count)
+    
+    #out <- paste("mean_plan_length@", domain_list[i],sep="")
+    #write.csv(d, out)
+  }
+  
+  write.csv(summary_table, output_file)
+  
+}
 
-#robust_plans_comparison(domain_list[5:6], file_for_lower_annotations_free_ff[5:6], file_for_default[5:6], "../summary_tables/lower_annotations_free_ff_VS_default.csv")
+time_for_last_plan_comparison <- function(domain_list,
+                                           file_for_first_approach,
+                                           file_for_second_approach, output_file) {
+  # Number of domains
+  N <- length(domain_list)
+  
+  summary_table <- data.table(domain=rep("", N), faster=rep(0, N),
+                              equal=rep(0, N), slower=rep(0, N))
+  row_count <- 0
+  
+  for (i in 1:N) {
+    # Read result files produced by each approach for this domain
+    first_table <- data.table(read.csv(file_for_first_approach[i], comment.char='#'))
+    second_table <- data.table(read.csv(file_for_second_approach[i], comment.char='#'))
+    
+    # Extract for each instance (incomplete domain + problem) the maximal robustness
+    time_last_plan_first_table <- first_table[, list(time_last_plan = max(total_time)), by=list(domain, problem)]
+    time_last_plan_second_table <- second_table[, list(time_last_plan = max(total_time)), by=list(domain, problem)]
+    
+    # Merge the two tables, only consider instances for which both approaches produce plans
+    d <- merge(time_last_plan_first_table, time_last_plan_second_table, by=c("domain","problem"))
+    faster_count = nrow(d[d$time_last_plan.x < d$time_last_plan.y])    
+    equal_count = nrow(d[d$time_last_plan.x == d$time_last_plan.y])
+    slower_count = nrow(d[d$time_last_plan.x > d$time_last_plan.y])
+    
+    row_count <- row_count + 1
+    summary_table[row_count,] <- list(domain_list[i],faster_count, equal_count, slower_count)
+    
+    #out <- paste("mean_plan_length@", domain_list[i],sep="")
+    #write.csv(d, out)
+  }
+  
+  write.csv(summary_table, output_file)
+  
+}
+
+mean_time_comparison <- function(domain_list,
+                                          file_for_first_approach,
+                                          file_for_second_approach, output_file) {
+  # Number of domains
+  N <- length(domain_list)
+  
+  summary_table <- data.table(domain=rep("", N), faster=rep(0, N),
+                              equal=rep(0, N), slower=rep(0, N))
+  row_count <- 0
+  
+  for (i in 1:N) {
+    # Read result files produced by each approach for this domain
+    first_table <- data.table(read.csv(file_for_first_approach[i], comment.char='#'))
+    second_table <- data.table(read.csv(file_for_second_approach[i], comment.char='#'))
+    
+    # Extract for each instance (incomplete domain + problem) the maximal robustness
+    mean_time_first_table <- first_table[, list(mean_time = mean(total_time)), by=list(domain, problem)]
+    mean_time_second_table <- second_table[, list(mean_time = mean(total_time)), by=list(domain, problem)]
+    
+    # Merge the two tables, only consider instances for which both approaches produce plans
+    d <- merge(mean_time_first_table, mean_time_second_table, by=c("domain","problem"))
+    faster_count = nrow(d[d$mean_time.x < d$mean_time.y])    
+    equal_count = nrow(d[d$mean_time.x == d$mean_time.y])
+    slower_count = nrow(d[d$mean_time.x > d$mean_time.y])
+    
+    row_count <- row_count + 1
+    summary_table[row_count,] <- list(domain_list[i],faster_count, equal_count, slower_count)
+    
+    #out <- paste("mean_plan_length@", domain_list[i],sep="")
+    #write.csv(d, out)
+  }
+  
+  write.csv(summary_table, output_file)
+  
+}
+
+#num_max_robustness_instances_comparison(domain_list[1:4], file_for_lower_annotations_free_ff[1:4], file_for_lower_inc_robust[1:4], "../summary_tables/num_max_robustness_instances@lower_annotations_free_ff_VS_inc_robust.csv")
+
+#num_max_robustness_instances_comparison(domain_list[5:6], file_for_default[5:6], file_for_lower_inc_robust[5:6], "../summary_tables/num_max_robustness_instances@default_VS_lower_inc_robust.csv")
+
+num_max_robustness_instances_comparison(domain_list[5:6], file_for_default[5:6], file_for_lower_annotations_free_ff[5:6], "../summary_tables/num_max_robustness_instances@default_VS_lower_annotations_free_ff.csv")
+
+#num_plans_comparison(domain_list[5:6], file_for_default[5:6], file_for_lower_inc_robust[5:6], "../summary_tables/num_plans@default_VS_inc_robust.csv")
+
+#num_plans_comparison(domain_list[1:4], file_for_lower_annotations_free_ff[1:4], file_for_lower_inc_robust[1:4],"../summary_tables/num_plans@lower_annotations_free_ff_VS_inc_robust.csv")
+
+#mean_plan_length_comparison(domain_list[5:6], file_for_default[5:6], file_for_lower_inc_robust[5:6], "../summary_tables/mean_plan_length@default_VS_lower_inc_robust.csv")
+
+#mean_plan_length_comparison(domain_list[1:4], file_for_lower_annotations_free_ff[1:4], file_for_lower_inc_robust[1:4], "../summary_tables/mean_plan_length@lower_annotations_free_ff_VSr_inc_robust.csv")
+
+#time_for_first_plan_comparison(domain_list[5:6], file_for_default[5:6], file_for_lower_inc_robust[5:6], "../summary_tables/time_first_plan@default_VS_lower_inc_robust.csv")
+
+#time_for_last_plan_comparison(domain_list[5:6], file_for_default[5:6], file_for_lower_inc_robust[5:6], "../summary_tables/time_last_plan@default_VS_lower_inc_robust.csv")
+
+#time_for_first_plan_comparison(domain_list[1:4], file_for_lower_annotations_free_ff[1:4], file_for_lower_inc_robust[1:4], "../summary_tables/time_first_plan@lower_annotations_free_ff_VS_inc_robust.csv")
+
+#time_for_last_plan_comparison(domain_list[1:4], file_for_lower_annotations_free_ff[1:4], file_for_lower_inc_robust[1:4], "../summary_tables/time_last_plan@lower_annotations_free_ff_VS_inc_robust.csv")
+
+mean_time_comparison(domain_list[5:6], file_for_default[5:6], file_for_lower_inc_robust[5:6], "../summary_tables/mean_time@default_VS_lower_inc_robust.csv")
+
+mean_time_comparison(domain_list[1:4], file_for_lower_annotations_free_ff[1:4], file_for_lower_inc_robust[1:4], "../summary_tables/mean_time@lower_annotations_free_ff_VS_inc_robust.csv")
